c7a735a62e36f3dc7aa56faea4dce649fb70d027
Toshihiro Shimizu <shimizu.toshihiro@gmail.com>
2012-07-02 22:37:27 +0900

    countまで

---
+++ app.js
@@ -0,0 +1,50 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes');
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// Routes
+
+app.get('/', routes.index);
+app.get('/chat', routes.chat);
+
+app.listen(3000, function(){
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
+
+var io = require('socket.io').listen(app);
+var chatCount = 0;
+var chat = io.of('/chat').on('connection', function(socket) {
+  chatCount++;
+  console.log(chatCount);
+  chat.emit('count', {count: chatCount});
+
+  socket.on('disconnect', function() {
+    chatCount--;
+    chat.emit('count', {count: chatCount});
+  });
+});
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' })
+};
+
+exports.chat = function(req, res) {
+  res.render('chat', {title: 'Chatサンプル'});
+};
